a5f7b5d6ab626f24836f9ee2b38aaa074a39243c
baroobarecked <aaron@brubecksecure.com>
2021-12-06 09:52:53 -0700

    added fontawesome

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -30,8 +30,8 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>Profile
-        {/* <i className="fas fa-user-circle" /> */}
+      <button onClick={openMenu}>
+        <i className="fas fa-user-circle" />
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
