64a3f16d1a71d2e6503b8f62911e78591d84a730
baroobarecked <aaron@brubecksecure.com>
2021-12-06 10:52:47 -0700

    added components and store files

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session';
+import notes from './notes';
+import notebooks from './notebooks';
 
 const rootReducer = combineReducers({
     session,
+    notes,
+    notebooks,
 });
 
 let enhancer;
